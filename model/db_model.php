29ed40d2e0ad5cd4f6aef4b748de660bb49c90b0
Pasanbasuru <basurupasan@gmail.com>
2017-12-12 16:35:49 +0530

    change back

--- model/db_model.php
+++ model/db_model.php
@@ -36,9 +36,9 @@ class DB {
      */
     public function connect() { 
 
-        $localhost = "sms.cqecy47qtkvz.ap-northeast-1.rds.amazonaws.com";
+        $localhost = "127.0.0.1";
         $username = "root";
-        $password = "rootpassword";
+        $password = "";
         $dbName = "sms";  
         
         // Try and connect to the database
