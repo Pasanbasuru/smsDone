2784e93b3dfc2cd58e24925c33b57adda9a0a3ef
Unknown <mohan.at145@gmail.com>
2017-12-12 17:44:28 +0530

    Auto stash before merge of "master" and "origin/master"
    ar added

---
+++ model/ar_acedemic_model.php
@@ -0,0 +1,70 @@
+<?php
+	if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
+        /* 
+           Up to you which header to send, some prefer 404 even if 
+           the files does exist for security
+        */
+        header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
+
+        /* choose the appropriate page to redirect users */
+        die( header( 'location: /error.php' ) );
+
+    }
+
+    class ArAcedemicModel{
+      protected static $db;
+
+      function __construct(){
+        self::$db = new DB();
+
+      } 
+
+      function view_ar_acedemic($aracedemic_id){
+        $query = "SELECT * FROM `ar_acedemic` WHERE username = ".$aracedemic_id." ";
+
+        $result = self::$db->select($query);
+
+        return $result;
+      }
+
+      function addevent($eventdate,$eventtitle,$eventdes,$fname){
+
+         $query="INSERT INTO events (eventdate,eventtitle,eventdes,file)
+
+         VALUES ($eventdate,$eventtitle,$eventdes,$fname)";
+          die($query);
+         $result = self::$db->query($query);
+
+          return $result;
+
+        }
+        function view_mahapola(){
+            $query = "SELECT * FROM `scholarship`
+              WHERE schol_type='Mahapola'";
+
+            $result = self::$db->select($query);
+
+            return $result;
+        }
+
+        function view_bursary(){
+            $query = "SELECT * FROM `scholarship`
+              WHERE schol_type='Bursary'";
+
+            $result = self::$db->select($query);
+
+            return $result;
+        }
+
+        function view_other_scholarship(){
+            $query = "SELECT * FROM `scholarship`
+              WHERE schol_type='Other'";
+
+            $result = self::$db->select($query);
+
+            return $result;
+        }
+
+
+    }
+?>
